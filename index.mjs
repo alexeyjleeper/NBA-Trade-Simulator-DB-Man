b09c0ef40091b0d1b1f2249b2ccde810ef667b38
Alexey Leeper <alexeyleeper@gmail.com>
2024-08-05 23:47:12 -0700

    added JSDoc comments for endpoints, classes, and class methods

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import { DynamoDBClient, GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import PlayerData from './storage/playerData.json' assert { type: 'json' };
+import TeamData from './storage/teamData.json' assert { type: 'json' };
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -11,37 +12,37 @@ const dbClient = new DynamoDBClient({
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const playerData = PlayerData["playerData"];
 
-
-// need a dict with team:starting_ovr
-
+/**
+ * @api {get} /search Get Players, Picks, and Score
+ * @apiDescription Returns a list of players, draft picks, and the current score for the provided team.
+ * @apiParam {String} uuid The unique identifier for the team.
+ * @apiParam {String} team The name of the team.
+ * @apiParam {Boolean} db Access the database if true, otherwise use local file storage.
+ * @apiSuccess {Object} response The response object.
+ * @apiSuccess {String[]} response.Players Array of player names.
+ * @apiSuccess {Object[][]} response.Picks Array of draft picks, where each pick is an array of [year, round, type].
+ * @apiSuccess {Number} response.Score The current score of the team.
+ * @apiError (500) {String} InternalServerError Internal Server Error message.
+ */
 app.get("/search", async (req, res) => {
-    // Description - Returns a list of players, draft picks, and the current score
-    // for the provided team
-    // @param {string} uuid
-    // @param {string} team
-    // @param {bool} db_access
-    // @returns {Promise<Object>} json object
-    //      - {Array} players array
-    //      - {Array} draft picks
-    //          - {Array} [year, round, "P" (protected) or "U" (unprotected)]
-    //      - {Int} score
-
     let players, picks, score;
-    const handler = GetDataHandler(req.query);
+    const handler = new GetDataHandler(req.query);
+    const db = req.query.db === 'true';
 
     if (!db) {
-        console.log('non-db');
+        [players, picks, score] = handler.fromFileStorage();
     } else {
         forDBGet = handler.formatForDBGet();
         let dbRes;
+
         try {
             dbRes = await dbClient.send(new GetItemCommand(forDBGet));
         } catch(err) {
             console.log(err);
             res.status(500).send('Error contacting DynamoDB: ', err);
         }
+
         [players, picks, score] = handler.dbResToEndpointRes(dbRes);
     }
 
@@ -57,16 +58,21 @@ app.get("/search", async (req, res) => {
     }
 });
 
-// request body is going to have to contain teams and the players to swap
+/**
+ * @api {put} /update Update Teams and Picks in DynamoDB
+ * @apiDescription Updates the database with new data for two teams involved in a trade and returns the current score of the user's team if involved in the trade.
+ * @apiParam {Object} req.body The request body.
+ * @apiParam {String} req.body.Uuid The unique identifier for the trade.
+ * @apiParam {String[]} req.body.TradeTeams Array of two team names involved in the trade.
+ * @apiParam {String[][]} req.body.NewRosters Array of two arrays representing the new rosters for each team post-trade.
+ * @apiParam {Object[][]} req.body.Picks Array of two arrays representing the draft picks for each team post-trade.
+ * @apiParam {String} req.body.Team The user's currently selected team.
+ * @apiSuccess {Number[]} score The updated score array for the user's team. Array contains [insideScoring, outsideScoring, athleticism, playmaking, rebounding, defending].
+ * @apiError (500) {String} InternalServerError Internal Server Error message.
+ */
 app.put("/update", async (req, res) => {
-    //params: uuid, new teams (post trade), new picks (post trade) players, curr user team
-    //returns: curr team score
-    const data = new PutDataHandler(req.body, res);
-    const [team1, team2] = data.formatForDBPut();
-    
-
-    //need to add a call to getCurrTeamScore
-
+    const handler = new PutDataHandler(req.body);
+    const [team1, team2] = handler.formatForDBPut();
 
     try {
         const [put_res1, put_res2] = Promises.all([
@@ -75,6 +81,13 @@ app.put("/update", async (req, res) => {
         ]);
     } catch(err) {
         console.log(err);
+        res.status(500).send("Error contacting DynamoDB: ", err);
+    }
+
+    try {
+        res.send(handler.getCurrTeamScore());
+    } catch(err) {
+        res.status(500).send("Internal Server Error: ", err);
     }
 });
 
@@ -82,6 +95,10 @@ app.listen(PORT, () => {
     console.log(`Player search server listening on port ${PORT}...`);
 });
 
+/**
+ * @class GetDataHandler
+ * @description Handles operations related to fetching data for a team.
+ */
 class GetDataHandler {
     constructor (query) {
         this.uuid = query.uuid;
@@ -89,10 +106,20 @@ class GetDataHandler {
         this.db = query.team === 'true';
     }
 
-    localBuildItems () {
-        console.log('non-db build');
+    /**
+     * @method fromFileStorage
+     * @description Retrieves data from local file storage.
+     * @returns {Array} Array containing players, picks, and score.
+     */
+    fromFileStorage() {
+        return [TeamData[this.team]["players"], TeamData[this.team]["picks"], TeamData[this.team]["score"]]
     }
 
+    /**
+     * @method formatForDBGet
+     * @description Formats the query for DynamoDB retrieval.
+     * @returns {Object} The formatted query object for DynamoDB.
+     */
     formatForDBGet () {
         return {
             "TableName" : "Roster_Data",
@@ -108,6 +135,12 @@ class GetDataHandler {
         }
     }
 
+    /**
+     * @method dbResToEndpointRes
+     * @description Converts DynamoDB response format to endpoint response format.
+     * @param {Object} dbObj The DynamoDB response object.
+     * @returns {Array} Array containing players, picks, and score.
+     */
     dbResToEndpointRes (dbObj) {
         const players = [];
         for (const item of dbObj.Item.Players.L) {
@@ -127,8 +160,12 @@ class GetDataHandler {
     }
 }
 
+/**
+ * @class PutDataHandler
+ * @description Handles operations related to updating data for teams involved in a trade.
+ */
 class PutDataHandler {
-    constructor (data, res) {
+    constructor (data) {
         this.uuid = data["Uuid"];
         this.tradeTeams = data["TradeTeams"];
         this.rosters = data["NewRosters"];
@@ -146,7 +183,9 @@ class PutDataHandler {
                     "Picks" : {
                         "L" : []
                     },
-                    "Score" : {}
+                    "Score" : {
+                        "L" : []
+                    }
                 },
                 "ReturnConsumedCapacity" : "TOTAL"
             },
@@ -161,21 +200,28 @@ class PutDataHandler {
                     "Picks" : {
                         "L" : []
                     },
-                    "Score" : {}
+                    "Score" : {
+                        "L" : []
+                    }
                 },
                 "ReturnConsumedCapacity" : "TOTAL"
             }
         ]
     }
 
+    /**
+     * @method getScore
+     * @description Calculates the score for a given team based on the top 8 players.
+     * @param {String} team The team for which to calculate the score.
+     * @returns {Number[]} Array containing the scores: [insideScoring, outsideScoring, athleticism, playmaking, rebounding, defending].
+     */
     getScore(team) {
         // get index for accessing rosters
         const accessIndex = this.tradeTeams.indexOf(team);
         
-        //if user's team is not in the trade, score will not
-        // have to be returned by the update endpoint
+        // if user's current team is not in trade, return empty array
         if (accessIndex === -1){
-            return 0;
+            return [];
         }
 
         let insideScoring = 0;
@@ -186,18 +232,17 @@ class PutDataHandler {
         let defending = 0;
         
         //get array of top 8 players by overall
-        const sortByOvr = this.rosters[accessIndex].slice().sort((a, b) => playerData[b][1] - playerData[a][1]);
+        const sortByOvr = this.rosters[accessIndex].slice().sort((a, b) => PlayerData[b][1] - PlayerData[a][1]);
         const top8 = sortByOvr.length > 7 ? sortByOvr.slice(0, 8) : sortByOvr;
         
         //score calculation
         top8.forEach(player => {
-            insideScoring += playerData[player][2];
-            console.log(`accumulated: ${insideScoring}`);
-            outsideScoring += playerData[player][3];
-            athleticism += playerData[player][4];
-            playmaking += playerData[player][5];
-            rebounding += playerData[player][6];
-            defending += playerData[player][7];
+            insideScoring += PlayerData[player][2];
+            outsideScoring += PlayerData[player][3];
+            athleticism += PlayerData[player][4];
+            playmaking += PlayerData[player][5];
+            rebounding += PlayerData[player][6];
+            defending += PlayerData[player][7];
         });
         const length = top8.length;
         insideScoring = Math.floor(insideScoring / length);
@@ -206,18 +251,25 @@ class PutDataHandler {
         playmaking = Math.floor(playmaking / length);
         rebounding = Math.floor(rebounding / length);
         defending = Math.floor(defending / length);
-        const scoreSum = (insideScoring + outsideScoring + athleticism + playmaking + rebounding + defending) * 10;
+        const scoreSum = [insideScoring, outsideScoring, athleticism, playmaking, rebounding, defending];
         return scoreSum;
     }
 
+    /**
+     * @method populatePutItem
+     * @description Populates the data to be put into DynamoDB for both teams involved in the trade.
+     */
     populatePutItem() {
-        //populate dictionaries for both teams in trade
+        
+        // populate dictionaries for both teams in trade
         for (let i = 0; i < 2; i++) {
             this.putData[i]["Item"]["Uuid"]["S"] = this.uuid;
             this.putData[i]["Item"]["Team"]["S"] = this.tradeTeams[i];
+
             for (const player of this.rosters[i]) {
                 this.putData[i]["Item"]["Players"]["L"].push({"S" : player});
             }
+
             for (const pick of this.picks[i]) {
                 this.putData[i]["Item"]["Picks"]["L"].push({
                     "L" : [
@@ -227,15 +279,29 @@ class PutDataHandler {
                     ]
                 });
             }
-            this.putData[i]["Item"]["Score"]["N"] = `${this.getScore(this.tradeTeams[i])}`;
+
+            const scoreArr = this.getScore(this.tradeTeams[i]);
+            for (const item of scoreArr) {
+                this.putData[i]["Item"]["Score"]["L"].append({ "N" : item})
+            }
         }
     }
 
+    /**
+     * @method formatForDBPut
+     * @description Formats the data for DynamoDB put operations.
+     * @returns {Object[]} Array of formatted data for both teams.
+     */
     formatForDBGet() {
         this.populatePutItem();
         return this.putData;
     }
 
+    /**
+     * @method getCurrTeamScore
+     * @description Retrieves the current score for the user's selected team.
+     * @returns {Number[]} The score array for the user's team.
+     */
     getCurrTeamScore() {
         return this.getScore(this.team);
     }
