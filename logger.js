51222e4cbefcb3b5754d298dd88a1c9c6e95c088
Alexey Leeper <alexeyleeper@gmail.com>
2024-08-30 11:30:33 -0700

    logger JSDoc comments

--- logger.js
+++ logger.js
@@ -3,9 +3,14 @@ import fs from 'fs';
 import { dirname, join } from 'path';
 import zlib from 'zlib';
 
+// get var for current dir
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+/**
+ * @class Logger
+ * @desscription Handles info and error message logging
+ */
 class Logger {
     constructor() {
         this.logdir = join(__dirname, 'logs');
@@ -16,11 +21,21 @@ class Logger {
         }
     }
     
+    /**
+     * @method getLogFilePath
+     * @description returns filepath, in logs file, for the file according to the date
+     * @returns {String} filepath of current file by date
+     */
     getLogFilePath() {
         const date = new Date().toISOString().split('T')[0];
         return join(this.logdir, `app-${date}.log`);
     };
     
+    /**
+     * @method compress
+     * @description compresses the current data of the file and creates new .gz file
+     * @param {String} filePath path of the file that will be compressed
+     */
     compress(filePath) {
         const input = fs.createReadStream(filePath);
         const gzip = zlib.createGzip();
@@ -28,6 +43,10 @@ class Logger {
         input.pipe(gzip).pipe(output);
     };
     
+    /**
+     * @method deleteOldLogs
+     * @description scans logs dir for old files and deletes both .log and .gz files
+     */
     deleteOldLogs() {
         fs.readdir(this.logdir, (error, files) => {
             if (error) throw error;
@@ -47,6 +66,10 @@ class Logger {
         })
     }
     
+    /**
+     * @method rotateLogs
+     * @description rotates current log file and removes any old files
+     */
     rotateLogs() {
         const filePath = this.getLogFilePath();
         if (fs.existsSync(filePath) && fs.statSync(filePath).size > this.maxFileSize) {
@@ -56,6 +79,12 @@ class Logger {
         }
     }
 
+    /**
+     * @method info
+     * @description adds info message to logs
+     * @param {String} message
+     * @param {Object} req dictionary containing all the queries from the client
+     */
     info(message, req=null) {
         this.rotateLogs();
 
@@ -69,6 +98,13 @@ class Logger {
         fs.appendFileSync(filepath, entryJSON + '\n', 'utf8');
     }
 
+    /**
+     * @method error
+     * @description adds error entry to logs
+     * @param {String} message 
+     * @param {String} error external caught error message
+     * @param {Object} stack 
+     */
     error(message, error, stack) {
         this.rotateLogs();
         const filepath = this.getLogFilePath;
